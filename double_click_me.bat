6fde8396937b58b6e3458a6a9fe81651e62a5a5e
Fara <fara@NPL>
2018-10-18 17:50:41 +0000

    Installation checking

--- double_click_me.bat
+++ double_click_me.bat
@@ -7,6 +7,10 @@ REM This should work as long as conda is installed. To see if this is the case,
 REM start a command prompt and write "conda". If you get an error message
 REM instead of a help display, it's not installed properly.
 
+REM See if it's already working
+call activate dataset_checker
+if errorlevel 1 (
+
 call conda env create --file georgie_dataset_env.yaml
 
 call activate dataset_checker
@@ -21,7 +25,17 @@ echo activate dataset_checker
 echo python ./safe_dataset_checker.py -h
 echo.
 echo You should get a friendly message if it worked. 
-echo.
-echo.
 
+) else (
+
+echo Seems like it's already installed. If you're having trouble, run
+echo.
+echo conda env remove --name dataset_checker
+echo.
+echo Then run this file again
+
+)
+
+echo.
+echo.
 pause
